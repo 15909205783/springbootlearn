4feaaaaa183f79f7013eaf00e3ed0dc471ac1435
fanYang <2585928225@qq.com>
2020-04-14 16:45:26 +0800

    集成rabbitmq

---
+++ springboot-mqdemo/src/main/java/com/yangfan/springbootmqdemo/SpringbootMqdemoApplication.java
@@ -0,0 +1,13 @@
+package com.yangfan.springbootmqdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMqdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMqdemoApplication.class, args);
+    }
+
+}
